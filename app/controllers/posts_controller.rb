fd30b81eb20f40cbd6976c39828f07f6031a0e51
yoheioda0127 <maikemi00romans@gamil.com>
2021-03-30 20:09:41 +0900

    実装準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
- def index
-   @posts = Post.all
- end
- 
- def new
- end
+  def index
+    @posts = Post.all.order(1d: "DESC")
+  end
 
- def create
-  Post.create(content: params[:content])
- end
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
 end
