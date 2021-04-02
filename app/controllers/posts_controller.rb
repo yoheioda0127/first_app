b7f4c40946edda143fa9c95b7484704727720374
yoheioda0127 <maikemi00romans@gamil.com>
2021-04-02 09:23:15 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,19 @@ class PostsController < ApplicationController
   def create
     Post.create(content: params[:content])
   end
+
+  def checked
+    post = Post.find(params[:id])
+      # 設定したURLパラメーターから、既読したメモのidが渡されるように設定.
+      # そのidを使用して該当するレコードを取得している。
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+      # ここまでで更新完了
+    item = Post.find(params[:id])
+    render json:{post: item}
+      # JSON形式でchecked.jsに返却してあげる
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
   # get 'posts',to:'posts#index'
+  get 'posts/:id', to: 'posts#checed'
 end
