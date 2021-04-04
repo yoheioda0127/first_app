f4e4c4103698c48ed332f75bbeb9a4ac12813486
yoheioda0127 <maikemi00romans@gamil.com>
2021-04-04 10:35:06 +0900

    メモ投稿機能

---
+++ app/javascript/memo.js
@@ -0,0 +1,35 @@
+function memo() {
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    const formData = new FormData(document.getElementById("form"));
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      }
+      const item = XHR.response.post;
+            // メモのレコードデータを取得
+      const list = document.getElementById("list");
+            // 「描画する親要素」のlistの要素を取得
+      const formText = document.getElementById("content");
+            // メモの入力フォームを取得（消去するため）
+      const HTML = `
+        <div class="post" data-id=${item.id}>
+          <div class="post-date">
+            投稿日時：${item.created_at}
+          </div>
+          <div class="post-content">
+          ${item.content}
+          </div>
+        </div>`;
+      list.insertAdjacentHTML("afterend", HTML);
+      formText.value = "";
+    };
+    e.preventDefault();
+  });
+}
+window.addEventListener("load", memo);
